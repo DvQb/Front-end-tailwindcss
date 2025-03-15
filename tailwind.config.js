ba4c41f3bebaf0a7169ec44208ae372adecb85a7
DvQb <davidresendiz753@gmail.com>
2025-03-15 14:33:51 -0600

    creation of nav-bar for desktop and mobile

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,28 @@
  export default {
     content: ["./src/**/*.{html,js}"],
       theme: {
-        extend: {},
+        extend: {
+
+          fontFamily: {
+            'principal' : [  '"Be Vietnam Pro"', 'sans-serif' ]
+          },
+          colors: {
+            "Bright-Red": "#F25F3A",
+            'Dark-Blue': 'hsl(228, 39%, 23%)',
+            'Dark-Grayish-Blue': 'hsl(227, 12%, 61%)',
+            'Very-Dark-Blue': 'hsl(233, 12%, 13%)',
+            'Very-Pale-Red': 'hsl(13, 100%, 96%)',
+            'Very-Light-Gray': 'hsl(0, 0%, 98%)',
+  
+          },
+          backgroundImage: {
+
+            'close-menu': "url('../img/icon-close.svg')",
+            'open-menu': "url('../img/icon-hamburger.svg')",
+          },
+        },
+       
+
         
       },
       plugins: [],
